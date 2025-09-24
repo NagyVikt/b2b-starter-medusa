f40adc333f40f51bc84018fdd4937068afe78eda
NagyVikt <nagy.viktordp@gmail.com>
2025-09-24 21:01:32 +0200

    fix

--- backend/medusa-config.ts
+++ backend/medusa-config.ts
@@ -15,6 +15,10 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
+    databaseDriverOptions: {
+      ssl: false,
+      sslmode: "disable",
+    },
   },
   modules: {
     [COMPANY_MODULE]: {
